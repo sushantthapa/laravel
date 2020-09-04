fb00bf71023418e9b941de2c1af62d2932863716
sushantthapa <sushant.thapa55@gmail.com>
2020-09-04 12:26:35 +0545

    commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+<div class="jumbotron text-center">
+    <div class="container">
+      <h1 class="display-3">Welcome To Laravel</h1>
+      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+      <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login &raquo;</a> <a class="btn btn-success btn-lg" href="/Register" role="button">Register &raquo;</a></p>
+    </div>
+  </div>
+
+@endsection
